35c70b97fb66ddf12fde6ee3d9c9c856488aec09
Márton Bán <30813844+martonban@users.noreply.github.com>
2023-03-21 14:53:07 +0100

    Hello Triangel - wip

--- 02 Shader Loader/src/main/java/engine/LevelEditorScene.java
+++ 02 Shader Loader/src/main/java/engine/LevelEditorScene.java
@@ -1,30 +1,13 @@
 package engine;
 
-import java.awt.event.KeyEvent;
 
 public class LevelEditorScene extends Scene {
 
-    private boolean changingScene = false;
-    private float timeToChangingScene = 2.0f;
-
     public LevelEditorScene() {
-        System.out.println("Inside Level Editor Scene");
     }
 
     @Override
     public void update(float dt) {
-        if(!changingScene && KeyListener.isKeyPressed(KeyEvent.VK_SPACE)){
-            changingScene = true;
-        }
-
-        if(changingScene && timeToChangingScene > 0){
-            timeToChangingScene -= dt;
-            Window.get().r -= dt * 5.0f;
-            Window.get().g -= dt * 5.0f;
-            Window.get().b -= dt * 5.0f;
-        } else if(changingScene){
-            Window.changeScene(1);
-        }
 
     }
 
